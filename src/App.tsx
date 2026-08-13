66777a44646f1594aa5ad1f410df528f2ca57aed
codeTukai <tukaighosh753@gmail.com>
2026-08-13 09:52:29 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-
-import { Button } from "./component/ui/Button"
-
+import { PlusIcons } from "./component/icons/PlusIcon";
+import { Button } from "./component/ui/Button";
+import "./index.css";
 function App() {
-
-
   return (
-    <>
-<Button />
-    </>
-  )
+    <div className="flex pb-4">
+      <div>
+        <Button
+          startIcon={<PlusIcons size="md" />}
+          endIcon={<PlusIcons size="lg" />}
+          variant="primary"
+          size="lg"
+          text="share"
+        />
+      </div>
+      <div className="pl-2">
+        <Button
+          startIcon={<PlusIcons size="md" />}
+          endIcon={<PlusIcons size="lg" />}
+          variant="secondary"
+          size="lg"
+          text="Add Content"
+        />
+      </div>
+      {/* <div><Button variant="primary" size="md" text="Add content" /></div>
+<span><Button variant="primary" size="lg" text="Button" /></span> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/component/ui/Button.tsx
+++ src/component/ui/Button.tsx
@@ -1,26 +1,48 @@
 
-import type { ReactNode } from "react";
+import type { ReactElement, ReactNode } from "react";
 
-export interface ButtonProps{
+ interface ButtonProps{
     variant: "primary" | "secondary"; 
     size: "sm" | "md" | "lg";
     text: string;
-    startIcon?: ReactNode;
+    startIcon?: ReactElement;
     endIcon?: ReactNode;
-    onClick: () => void;
+    onClick?: () => void;
 }
+
 
 
 const variantStyles = {
-    "primary": "bg-purple-600 text-white",
-    "secondary" : "bg-purple-400 text-purple-600"
+    "primary": "bg-blue-400 text-white",
+    "secondary" : "bg-purple-200 text-purple-600"
 }
+
+// const sizeStyles = {
+//     "lg" : "p-6 ",
+//     "md": "p-4",
+//     "sm": "p-2"
+// }
+const sizeStyles = {
+    "lg" : "py-6 px-10 text-lg flex items-center",
+    "md": "py-2 px-4 text-md",
+    "sm": "py-1 px-2 text-sm"
+}
+
+const defaultStyle = "rounded-xl box-shadow"
 export const Button = (props:ButtonProps) => {
     return(
-        <button className={variantStyles[props.variant]}>
-           {props.text}
+        <button className= {`${sizeStyles[props.size]} ${variantStyles[props.variant]} ${defaultStyle}`}>
+            {props.startIcon}
+            <div className="flex pl-2 pr-2">
+                {props.text}
+            </div>
+             
+            {props.endIcon}
         </button>
+     
+       
+        
     )
 }
 
-<Button variant="primary" size="md" text="ff" onClick={()=>{}} startIcon="+" endIcon="-"/>
+{/* <Button variant="primary" size="md" text="ff" onClick={()=>{}} startIcon="+" endIcon="-"/> */}
